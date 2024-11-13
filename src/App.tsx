07352aca10ad81c8245336e5dfcd0e0764d0ba70
Rodrigo Morales <rodrigomv29@gmail.com>
2024-11-13 12:59:46 -0500

    page under construction message

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
-import * as React from 'react';
+import React, {useState} from 'react';
 import HelloWorld from './HelloWorld';
+import LoginPage from './components/Login';
 
 const App: React.FC = () => {
-  return (
-    <div>
-      <HelloWorld name="World" />
-    </div>
-  );
+    const [user, setUser] = useState(undefined)
+    if(!user){
+        return <LoginPage onLogin={function (email: string, password: string): void {
+            throw new Error('Function not implemented.');
+        } }></LoginPage>
+    }
+    else{
+        return (
+            <div>
+              <HelloWorld name="Rodrigo Morales (PAGE UNDER CONSTRUCTION..)" />
+            </div>
+          );
+
+    }
+
+  
 };
 
 export default App;
--- src/HelloWorld.tsx
+++ src/HelloWorld.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react';
 
 interface Props {
-  name: string;
+  age: string;
 }
 
-const HelloWorld: React.FC<Props> = ({ name }) => {
-  return <h1>Hello, {name}!</h1>;
+const HelloWorld: React.FC<Props> = ({ age }) => {
+  return <h1>Hello, {age}!</h1>;
+
 };
 
 export default HelloWorld
