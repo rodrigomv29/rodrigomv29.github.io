a4252b8bd16649f5fa7c9eb2a7490fbd84d1274d
Rodrigo Morales <rodrigomv29@gmail.com>
2024-12-11 20:53:13 -0500

    implmeneting zen quote

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 let lastUpdatedSpan = document.querySelector(".last-updated").textContent = new Date(document.lastModified).toLocaleDateString()
+async function getQuoteData() {
+    const url = "https://zenquotes.io/api/random"
+    try {
+        const response = await fetch(url);
+        if(!response.ok){
+            throw new Error(`Response status ${response.status}`)
+        }
+        const json = await response.json()
+    } catch(error){
+        console.error(error.message)
+    }
+    return json;
+}
