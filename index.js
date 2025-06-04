c5687a2e92b459ebce4149aee90d42f1a31187d0
Rodrigo Morales <rodrigomv29@gmail.com>
2025-06-04 10:06:57 -0400

    302 redirect error

--- index.js
+++ index.js
@@ -1,19 +1,58 @@
 let lastUpdatedSpan = document.querySelector(".last-updated").textContent = new Date(document.lastModified).toLocaleDateString()
-async function getQuoteData() {
-    const url = "https://zenquotes.io/api/random"
+const api_url ="http://zenquotes.io/api/random/";
+console.log("HELLO WORLD!!")
+async function getapi(url)
+{
     try {
-        const response = await fetch(url);
+        const response = await fetch(url, {mode: 'cors'});
         if(!response.ok){
-            throw new Error(`Response status ${response.status}`)
+            if (response.status == 429){
+                return await quote_limit()
+            }
+            else {
+                throw new Error(`HTTP error! status: ${response.status}`);
+
+            }
         }
-        const json = await response.json()
-    } catch(error){
-        console.error(error.message)
+        var data = await response.json();
+        return data;
+
     }
-    return json;
+    catch (error){
+        console.error('Error: ', error)
+    }
 }
-const nowMonth = Date.now()
-if (nowMonth.getFullYear() == "11"){
-    // read first line of used-quote    
+async function main() {
+    const data = await getapi(api_url);
+    if(data && data[0] && data[0]['q'] && data[0]['a']) {
+        console.log(data[0]['q']);
+        console.log(data[0]['a']);
+        return 'FUNNY'
+        
+    }
+    else  {
+        return "none"
+    }   
+   
+}
+
+let quote_text = document.querySelector(".quote-msg").textContent = main()
+
+
+async function quote_limit() {
+    return "quote limit reached";
+}
+
+/**
+ * 
+ * 
+ * 
+ * const now = new Date()
+ * console.log(now.getHours())
+    if (now.getFullYear() == "2025"){
+    // read first line of used-quote
+    console.log("Hello World!")
         
 }
+ * 
+ */
